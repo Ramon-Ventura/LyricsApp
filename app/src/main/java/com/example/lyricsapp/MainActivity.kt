b58c67692ed3c526573c9af9a1b26c6f4f789661
Ramón Ventura <50932514+Ramon-Ventura@users.noreply.github.com>
2019-12-20 12:10:51 -0600

    correccion

--- app/src/main/java/com/example/lyricsapp/MainActivity.kt
+++ app/src/main/java/com/example/lyricsapp/MainActivity.kt
@@ -2,17 +2,7 @@ package com.example.lyricsapp
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.android.volley.Request
-import com.android.volley.Response
-import com.android.volley.toolbox.StringRequest
-import com.android.volley.toolbox.Volley
-import com.example.lyricsapp.apiresponse.ApiResponse
-import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
